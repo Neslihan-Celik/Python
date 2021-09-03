93edad709a8873fa2b11b8e877aedfc283bfd335
Neslihan CELİK <nescelik96@gmail.com>
2021-09-03 11:40:08 +0300

    qr kod uluşturucu uygulaması geliştirildi

---
+++ qr_kod_olusturucu/qr_kod_olusturucu.py
@@ -0,0 +1,14 @@
+import qrcode
+
+qr=qrcode.QRCode(version=1,
+    error_correction =qrcode.constants.ERROR_CORRECT_L,
+    box_size=20,
+    border=5)
+
+
+giden_veri="Merhaba Ben NES bu qr kodu gorup kaynak koduna ulasmak isterseniz eger su linke tiklayabilirsiniz .."
+qr.add_data(giden_veri)
+qr.make(fit=True)
+
+resim=qr.make_image(fill_color="black",back_color="white")
+resim.save("qr_code.png")
